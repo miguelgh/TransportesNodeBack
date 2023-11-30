35bd35e6b4a761694b22fbddff88df2dec230e28
miguelgh16 <miguelgh16@gmail.com>
2023-11-29 22:22:38 -0300

    se modifica el tamaño de imagen

--- routes/api.js
+++ routes/api.js
@@ -10,8 +10,8 @@ router.get('/novedades', async function (req, res, next){
     novedades = novedades.map(novedades =>{
         if(novedades.img_id){
             const imagen = cloudinary.url(novedades.img_id, {
-                width: 960,
-                height: 200,
+                width: 500,
+                height: 500,
                 crop: 'fill'
             });
             return {
